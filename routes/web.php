953a79efa30c23e607add0671953e4c81b7bb6d4
YashPatel4268 <142138437+YashPatel4268@users.noreply.github.com>
2025-12-15 11:59:20 +0530

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 // Home route
-Route::get('/', function () {
+Route::get('/home', function () {
 return view('home');
 });
 
